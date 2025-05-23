1956b7ad61910fb7a3c68dab20117601cba3a617
Sai Uppalapati <saianirudhvarmauppalapati@gmail.com>
2025-05-23 16:02:19 -0400

    added test scripts

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,16 @@
-def test_always_passes():
-    assert True
+from app import app
+
+def test_home_status_code():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+
+def test_home_content():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert b"Hello from CI/CD pipeline!" in response.data
+
+def test_content_type():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.content_type == "text/html; charset=utf-8"
